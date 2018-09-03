055408cdac54b4e976565da5cd0c0a7f95d0a9f9
cosmos2 <playnstop.s@gmail.com>
2018-09-03 20:03:54 +0900

    static fin

--- src/components/UserStatistics.js
+++ src/components/UserStatistics.js
@@ -1,13 +1,220 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import { Grid, Row, Col } from "react-bootstrap";
+import {
+  BarChart,
+  ComposedChart,
+  Line,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from "recharts";
+import axios from "axios";
+import SocketIOClient from "socket.io-client";
+import "./UserStatistics.css";
+
+const cumulativeAccessData = [
+  { name: "Mon", day: 800 },
+  { name: "Tue", day: 967 },
+  { name: "Wed", day: 1098 },
+  { name: "Thu", day: 1200 },
+  { name: "Fri", day: 1108 },
+  { name: "Sat", day: 680 },
+  { name: "Sun", day: 680 }
+];
+const resigerData = [
+  { name: "Mon", sum: 4000, day: 2400 },
+  { name: "Tue", sum: 3000, day: 1398 },
+  { name: "Wed", sum: 2000, day: 9800 },
+  { name: "Thu", sum: 2780, day: 3908 },
+  { name: "Fri", sum: 1890, day: 4800 },
+  { name: "Sat", sum: 2390, day: 3800 },
+  { name: "Sun", sum: 3490, day: 4300 }
+];
 
 class UserStatistics extends Component {
-    render() {
-        return (
-            <div>
-                UserStatistics
-            </div>
-        );
+  constructor(props) {
+    super(props);
+    // 접속정보 받아오는 용도
+    // this._socket = SocketIOClient("http://catadmin.gq", {
+    //   query: this.props.token
+    // });
+
+    this.state = {
+      currentAccess: 100,
+      cumulativeDay: 234,
+      cumulativeWeek: 1203,
+      isAuthorized: true,
+      windowWidth: 1000
+    };
+  }
+
+  _checkLogin = async () => {
+    try {
+      if (localStorage.token) {
+        const response = await axios({
+          method: "GET",
+          url: "http://localhost:8080/api/users/1",
+          headers: { Authorization: "Bearer " + localStorage.token }
+        });
+        if (response.data.message === "Unauthorized") {
+          this.setState({
+            isAuthorized: false
+          });
+        }
+      } else {
+        this.setState({
+          isAuthorized: false
+        });
+      }
+    } catch (err) {
+      console.log(err);
     }
+  };
+  _sizeDetector = () => {
+    this.setState({
+      windowWidth: window.innerWidth - 200
+    });
+  };
+
+  async componentDidMount() {
+    await this._checkLogin();
+    await window.addEventListener("resize", this._sizeDetector);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this._sizeDetector);
+  }
+  render() {
+    const {
+      currentAccess,
+      cumulativeDay,
+      cumulativeWeek,
+      isAuthorized,
+      windowWidth
+    } = this.state;
+    return (
+      <div>
+        {isAuthorized ? (
+          <div>
+            <Grid>
+              <Row className="show-grid">
+                <Col xs={12} className="user-title">
+                  <h1>접속 및 고양이 등록 통계</h1>
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={2} />
+                <Col xs={8}>
+                  <div className="summary">
+                    <div className="currentAccess">
+                      현재 접속자 : {currentAccess}
+                    </div>
+                    <div className="cumulativeDay">
+                      금일 누적 접속 : {cumulativeDay}
+                    </div>
+                    <div className="cumulativeWeek">
+                      금주 누적 접속 : {cumulativeWeek}
+                    </div>
+                  </div>
+                </Col>
+                <Col xs={2} />
+              </Row>
+              <Row>
+                <Col xs={12}>
+                  <div className="charts">
+                    <div className="cumulativeAccessChart">
+                      <div className="title">누적 회원 통계</div>
+                      <ComposedChart
+                        width={windowWidth}
+                        height={400}
+                        data={resigerData}
+                        margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
+                      >
+                        <CartesianGrid stroke="#f5f5f5" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="day" barSize={20} fill="#413ea0" />
+                        <Line type="monotone" dataKey="sum" stroke="#ff7300" />
+                      </ComposedChart>
+                    </div>
+                    <div className="registerChart">
+                      <div className="title">접속 통계</div>
+                      <BarChart
+                        width={windowWidth}
+                        height={400}
+                        data={cumulativeAccessData}
+                        margin={{ top: 20, right: 20, left: 20, bottom: 20 }}
+                      >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="day" fill="#8884d8" />
+                        {/* <Bar dataKey="sum" fill="#82ca9d" /> */}
+                      </BarChart>
+                    </div>
+                  </div>
+                </Col>
+              </Row>
+            </Grid>
+          </div>
+        ) : (
+          <Redirect to="/unauthorized" />
+        )}
+      </div>
+    );
+  }
 }
 
 export default UserStatistics;
+
+/* <div className="container">
+<div className="summary">
+<div className="currentAccess">현재 접속자 : {currentAccess}</div>
+<div className="cumulativeDay">금일 누적 접속 : {cumulativeDay}</div>
+<div className="cumulativeWeek">금주 누적 접속 : {cumulativeWeek}</div>
+</div>
+<div className="charts">
+<div className="cumulativeAccessChart">
+<div className="title">누적 회원 통계</div>
+<ComposedChart
+width={1000}
+height={400}
+data={resigerData}
+margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
+>
+<CartesianGrid stroke="#f5f5f5" />
+<XAxis dataKey="name" />
+<YAxis />
+<Tooltip />
+<Legend />
+<Bar dataKey="day" barSize={20} fill="#413ea0" />
+<Line type="monotone" dataKey="sum" stroke="#ff7300" />
+</ComposedChart>
+</div>
+<div className="registerChart">
+<div className="title">접속 통계</div>
+<BarChart
+width={1000}
+height={400}
+data={cumulativeAccessData}
+margin={{ top: 20, right: 20, left: 20, bottom: 20 }}
+>
+<CartesianGrid strokeDasharray="3 3" />
+<XAxis dataKey="name" />
+<YAxis />
+<Tooltip />
+<Legend />
+<Bar dataKey="day" fill="#8884d8" />
+<Bar dataKey="sum" fill="#82ca9d" />
+</BarChart>
+</div>
+</div>
+</div> */
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,50 +1,121 @@
-import React, { Component } from 'react';
-import { Carousel, Grid, Row, Col } from 'react-bootstrap'
-import './Welcome.css'
+import React, { Component } from "react";
+import { Carousel, Grid, Row, Col } from "react-bootstrap";
+import "./Welcome.css";
 
 class Welcome extends Component {
-    render() {
-        return (
-            <div>
-                <Grid>
-                    <Row className="show-grid">
-                        <Col md={12}>
-                            <Carousel interval={500000}>
-                                <Carousel.Item className="a">
-                                   <img width={320} height={120} alt="320x320" src="/assets/image.png" />
-                                    <h3>반갑다옹 {this.props.username} 집사</h3>                                   
-                                </Carousel.Item>
-                                <Carousel.Item>
-                                    <img width={320} height={320} alt="320x320" src="/assets/cat1.png" />
-                                    <h3>1번냥이</h3>
-                                </Carousel.Item>
-                                <Carousel.Item>
-                                    <img width={320} height={320} alt="320x320" src="/assets/cat2.png" />
-                                    <h3>2번냥이</h3>
-                                </Carousel.Item>
-                                <Carousel.Item>
-                                    <img width={320} height={320} alt="320x320" src="/assets/cat3.png" />
-                                    <h3>3번냥이</h3>
-                                </Carousel.Item>
-                                <Carousel.Item>
-                                    <img width={320} height={320} alt="320x320" src="/assets/cat4.png" />
-                                    <h3>4번냥이</h3>
-                                </Carousel.Item>
-                                <Carousel.Item>
-                                    <img width={320} height={320} alt="320x320" src="/assets/cat5.png" />
-                                    <h3>5번냥이</h3>
-                                </Carousel.Item>
-                                <Carousel.Item>
-                                    <img width={320} height={320} alt="320x320" src="/assets/cat6.png" />
-                                    <h3>6번냥이</h3>
-                                </Carousel.Item>
-                            </Carousel>
-                        </Col>
-                    </Row>
-                </Grid>
-            </div>
-        );
+  constructor(props) {
+    super(props);
+    // 접속정보 받아오는 용도
+    // this._socket = SocketIOClient("http://catadmin.gq", {
+    //   query: this.props.token
+    // });
+
+    this.state = {
+      currentAccess: 100,
+      cumulativeDay: 234,
+      cumulativeWeek: 1203,
+      isAuthorized: true,
+      windowWidth: 1000,
+      text: "",
+      boardItem: {}
+    };
+  }
+  handleChange = e => {
+    this.setState({ text: e.target.value });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    if (this.state.text) {
+      const { username } = this.props;
+      const { text } = this.state;
+      const newIssue = { username, text, date: new Date().getTime() };
+      this.setState(prevState => {
+        const id = newIssue.date;
+        const newState = {
+          ...prevState,
+          boardItem: {
+            ...prevState.boardItem,
+            [id]: newIssue
+          },
+          text: ""
+        };
+        return { ...newState };
+      });
     }
+  };
+  render() {
+    const {
+      cumulativeDay,
+      cumulativeWeek,
+      currentAccess,
+      boardItem,
+      text
+    } = this.state;
+    console.log(this.state.boardItem);
+    return (
+      <div>
+        <Grid>
+          <Row className="show-grid">
+            <Col md={12}>
+              <Carousel interval={500000}>
+                <Carousel.Item className="a">
+                  <img
+                    width={320}
+                    height={120}
+                    alt="320x320"
+                    src="/assets/image.png"
+                  />
+                  <h3>반갑다옹 {this.props.username} 집사</h3>
+                </Carousel.Item>
+                <Carousel.Item className="summaryItem">
+                  <h3>통계 요약</h3>
+                  <div className="homeSummary">
+                    <div className="currentAccess">
+                      현재 접속자 : {currentAccess}
+                    </div>
+                    <div className="cumulativeDay">
+                      금일 누적 접속 : {cumulativeDay}
+                    </div>
+                    <div className="cumulativeWeek">
+                      금주 누적 접속 : {cumulativeWeek}
+                    </div>
+                  </div>
+                </Carousel.Item>
+                <Carousel.Item className="noticeItem">
+                  <h3>이슈보드</h3>
+                  <div className="noticeBoard">
+                    <ul className="board-list">
+                      {Object.keys(boardItem).map(key => (
+                        <li key={boardItem[key].date} className="board-item">
+                          {boardItem[key].username} : {boardItem[key].text}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                  <form className="boardInput">
+                    <input
+                      type="text"
+                      className="notice-input"
+                      value={text}
+                      maxLength="50"
+                      onChange={this.handleChange}
+                    />
+                    <button
+                      type="submit"
+                      onClick={this.handleSubmit}
+                      className="notice-submit"
+                    >
+                      submit
+                    </button>
+                  </form>
+                </Carousel.Item>
+              </Carousel>
+            </Col>
+          </Row>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default Welcome;
